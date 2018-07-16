86b986cdca339216ca68de553d0421d4317166b7
Syed Musa Raza <37310217+SMUSARAZA@users.noreply.github.com>
2018-07-16 22:20:18 +0200

    Updated the code with sensors.

--- Module_Dust-Sensor/dustSensor/dustSensor.ino
+++ Module_Dust-Sensor/dustSensor/dustSensor.ino
@@ -1,67 +1,80 @@
-/*
- Standalone Sketch to use with a Arduino Fio and a 
- Sharp Optical Dust Sensor GP2Y1010AU0F
- 
- Blog: http://arduinodev.woofex.net/2012/12/01/standalone-sharp-dust-sensor/
- Code: https://github.com/Trefex/arduino-airquality/
+#define trigPin 10
+#define echoPin 13
 
- For Pin connections, please check the Blog or the github project page
- Authors: Cyrille Médard de Chardon (serialC), Christophe Trefois (Trefex)
- Changelog:
-   2012-Dec-01: Cleaned up code
-   2012-Dec-13: Converted mg/m3 to ug/m3 which seems to be the accepted standard
-
- This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
- To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter 
- to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA. 
- */
- 
 int measurePin = 5;
 int ledPower = 12;
-
+ 
 int samplingTime = 280;
 int deltaTime = 40;
 int sleepTime = 9680;
-
+ 
 float voMeasured = 0;
 float calcVoltage = 0;
 float dustDensity = 0;
-
+ 
 void setup(){
   Serial.begin(9600);
   pinMode(ledPower,OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
-
-
-
-
+ 
 void loop(){
+  float duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) * 0.0344;
+  
+  if (distance >= 19){
+    Serial.print("Distance = ");
+    Serial.println("LAUNDRY BUCKET IS EMPTY. :)");
+  }
+  if (distance <=5){
+    Serial.print("Distance = ");
+    Serial.println("LAUNDRY BUCKET IS FULL.YOU NEED TO DO LAUNDRY. -_-");
+  }
+  if (distance < 19 && distance > 5){
+    Serial.print("Distance = ");
+    Serial.println("LAUNDRY BUCKET IS HALF FULL. :)");
+  }
+
+  delay(900);
+  
   digitalWrite(ledPower,LOW); // power on the LED
   delayMicroseconds(samplingTime);
-
+ 
   voMeasured = analogRead(measurePin); // read the dust value
-  
+ 
   delayMicroseconds(deltaTime);
   digitalWrite(ledPower,HIGH); // turn the LED off
   delayMicroseconds(sleepTime);
-
-  // 0 - 5.0V mapped to 0 - 1023 integer values 
-  calcVoltage = voMeasured * (5.0 / 1024); 
+ 
   
-  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
-  // Chris Nafis (c) 2012
-  dustDensity = (0.17 * calcVoltage - 0.1)*1000; 
+  calcVoltage = voMeasured * (5.0 / 1024);
+ 
   
+  dustDensity = (0.17 * calcVoltage - 0.1)*1000;
+ 
   Serial.print("Raw Signal Value (0-1023): ");
   Serial.print(voMeasured);
-  
+ 
   Serial.print(" - Voltage: ");
   Serial.print(calcVoltage);
-  
+ 
   Serial.print(" - Dust Density [ug/m3]: ");
   Serial.println(dustDensity);
+
+   if(dustDensity > 239){
+    Serial.println("Dust level is rising.");
+  }
   
+ 
   delay(1000);
 }
-
 
